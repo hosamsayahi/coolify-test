de08fca70e23d6f965cffdcfd786621a7ea0b71d
hosam <hosamedgesayahi@gmail.com>
2025-12-04 18:21:21 +0330

    feat: add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import express from 'express';
+import { prisma } from './db';
 
 const app = express();
 
 app.get('/', (_req, res) => {
    res.send("hello from coolify");
+});
+
+app.get('/get-users', async (_req, res) => {
+   const users = await prisma.user.findMany({
+      select: {
+         id: true,
+         username: true,
+      }
+   });
+
+   res.json({ users });
+});
+
+app.get('/add-users', async (_req, res) => {
+   const user = await prisma.user.findFirst({});
+
+   if (user) {
+      res.send("users already exist");
+      return;
+   }
+
+   await prisma.user.createMany({
+      data: [
+         { username: "hosamsayahi" },
+         { username: "personperson" },
+         { username: "anyone" },
+      ]
+   });
+
+   res.send("created users successfuly");
 });
 
 app.listen(3000, () => console.log("http://localhost:3000"));
